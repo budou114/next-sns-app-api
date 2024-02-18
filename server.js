c9ad37e934e3c20ce0588230d317f89514de3490
budou114 <58349030+budou114@users.noreply.github.com>
2024-02-18 23:26:09 +0900

    14. パスワードをbcryptライブラリを使ってハッシュ化してみよう

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
 
 const PORT = 5050;
 
-const prisma = PrismaClient();
+const prisma = new PrismaClient();
 
 // 新規ユーザー登録API
 app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
 
   const user = await prisma.user.create({
     data: {
       username,
       email,
-      password
+      password: hashedPassword,
     }
   });
 
